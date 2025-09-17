74892a770ba0ad54bdc2897a1e051a5b930e2c51
mp <pavl.mikhail@gmail.com>
2025-09-17 01:40:34 -0700

    Small fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,8 @@ export default defineNuxtConfig({
             link: [
                 { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
             ]
-        }
+        },
+        baseURL: '/'
     },
 
     // SSG configuration for GitHub Pages
